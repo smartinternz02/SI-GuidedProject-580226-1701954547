38a413b5eb59265b5f5c5bcb1680bd1e3469bc78
Aditya Harsh <98232917+Thakur-Aditya-Harsh@users.noreply.github.com>
2023-12-07 18:59:28 +0530

    Add files via upload

---
+++ Diceroller/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,165 @@
+package com.example.dicerollerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerAppTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Preview
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .wrapContentSize(align = Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    )
+
+
+    {
+        Text(
+            text = "ROLL THE DICE",
+            fontFamily = FontFamily.Serif,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline,
+            fontSize = 35.sp,
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 50.dp)
+
+
+        )
+
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString()
+        )
+        Column(
+            modifier = Modifier
+                .padding(vertical = 20.dp)
+                .padding(bottom = 10.dp)
+
+        ) {
+            val resultingNo = when (result) {
+                1 -> Text(
+                    color = Color.White,
+                    text = "You rolled a 1",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                2 -> Text(
+                    color = Color.White,
+                    text = "You rolled a 2",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                3 -> Text(
+                    color = Color.White,
+                    text = "You rolled a 3",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                4 -> Text(
+                    color = Color.White,
+                    text = "You rolled a 4",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                5 -> Text(
+                    color = Color.White,
+                    text = "You rolled a 5",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                else -> Text(
+                    color = Color.White,
+                    text = "You rolled a 6",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+        Button(onClick = { result = (1..6).random() }) {
+            Text("Roll")
+            Spacer(modifier = Modifier.width(width = 6.dp))
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Roll"
+            )
+        }
+
+
+    }
+}
+
+
